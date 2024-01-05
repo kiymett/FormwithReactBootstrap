1e7f7833434afa7be1925c4fd7776e422a90bfde
kiymettt <leylakkdemir@gmail.com>
2024-01-05 20:44:17 +0100

    test

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -2,27 +2,44 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 import {useState} from 'react';
 
 const Forms = () => {
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [url, setURL]= useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [firstname, setFirstname] = useState("");
+  // const [lastname, setLastname] = useState("");
+  // const [url, setURL]= useState("");
+  // const [password, setPassword] = useState("");
   const [showPass, setshowPass] = useState(false);
+  const [submit, setSubmit]= useState(false);
 
+  const [data, setData] = useState({
+    email: "",
+    username:"",
+    firstname:"",
+    lastname:"",
+    url:"",
+    password:"",
+  });
 
-  const handleEmail = (e) => {
-    console.log(e.target.value)
-    setEmail(e.target.value)
+  const {email, username, firstname, lastname, url, password} = data
+  
+  const handleData = (e) => {
+    setData ({...data, [e.target.id]: e.target.value})
   }
+ 
+  // const handleEmail = (e) => {
+  //   console.log(e.target.value)
+  //   setEmail(e.target.value)
+  // }
 
-  const handleUrl = (e) => {
-    console.log(e.target.value)
-    setURL(e.target.value)
-  }
+  // const handleUrl = (e) => {
+  //   console.log(e.target.value)
+  //   setURL(e.target.value)
+  // }
 
   const isEmailValid = () => {
     const atIndex = email.indexOf('@')
@@ -63,38 +80,42 @@ const Forms = () => {
   }
   return false
   }
+
   const handleShowPass = () => {
     setshowPass(!showPass);
     console.log("click");
     console.log(!showPass);
   }
   
-  const handleUsername = (e) => {
-    console.log(e.target.value)
-    setUsername(e.target.value)
-  }
+  // const handleUsername = (e) => {
+  //   console.log(e.target.value)
+  //   setUsername(e.target.value)
+  // }
 
   const handleFormSubmit = (e) => {
     console.log(e.target)
     e.preventDefault()
-    alert("form submit")
-    setEmail("");
-    setUsername("");
-    setFirstname("");
-    setLastname("");
-    setURL("");
-    setPassword("");
+    alert("form submit");
+    setData ({email:"", username :"", firstname:"", lastname:"", url:"", password:""})
+
   }
+  
+  // const handleSubmit=() => {
+  //   setSubmit(true)
+  //   console.log(data)
+  
+
+  // }
 
   return (
     <Container className ="container mt-4">
       <h1 className="text-success">FORM</h1>
-      <Form onSubmit={handleFormSubmit}>
+      <Form onSubmit={handleFormSubmit} >
       <Form.Group className="mb-3" >
         <Form.Label>Email address: <span>{email}</span></Form.Label>
         <Form.Control type="email" 
         placeholder="Enter email" 
-        onChange={handleEmail}
+        onChange={handleData}
         value= {email}
         id="email"
         required
@@ -105,7 +126,7 @@ const Forms = () => {
       <Form.Group className="mb-3" >
         <Form.Label>Username: {username}</Form.Label>
         <Form.Control type="text" placeholder="Enter User name" 
-        onChange={handleUsername}
+        onChange={handleData}
         value={username}
         id="username"/>
          {isUserName() ? <span className ="text-success">ok</span> : <span className = "text-danger">please type your username</span>}
@@ -114,7 +135,7 @@ const Forms = () => {
       <Form.Group className="mb-3" >
         <Form.Label>First Name: {firstname}</Form.Label>
         <Form.Control type="text" placeholder="Enter First Name"
-        onChange={(e)=>setFirstname(e.target.value)} 
+        onChange={handleData} 
         value={firstname}
         id="firstname"/>
         {isFirstName() ? <span className='text-success'>ok</span> : <span className='text-danger'>please type your firstname</span>}
@@ -123,7 +144,7 @@ const Forms = () => {
       <Form.Group className="mb-3" >
         <Form.Label>Last Name:{lastname} </Form.Label>
         <Form.Control type="text" placeholder="Enter Last Name" 
-        onChange = {(e)=>setLastname(e.target.value)}
+        onChange = {handleData}
         value={lastname}
         id="lastname"/>
         {isLastName() ? <span className ="text-success">ok</span> : <span className = "text-danger">please type your lastname</span>}
@@ -135,7 +156,7 @@ const Forms = () => {
         <Form.Control type="text" placeholder="Enter Image URL" 
         accept=".jpg, .jpeg, .png, .gif"
         // pattern="https?://.+\.(jpg|jpeg|png|gif)"
-        onChange={handleUrl}
+        onChange={handleData}
         value={url}
         name="url"
         id="url"/>
@@ -146,7 +167,7 @@ const Forms = () => {
           <Form.Control placeholder="Password" 
           type = {showPass? "text" : "password"}
           maxLength="8"
-          onChange={(e)=>setPassword(e.target.value)}
+          onChange={handleData}
           value={password}
           id="password"/>
           <Button  className ="" variant="dark" onClick={()=>handleShowPass()}>
@@ -156,15 +177,30 @@ const Forms = () => {
       <div className="container">
         <div className="row">
           <div className="col text-center">
-          <Button  className = "text-center" variant="primary" type="submit">
+          <Button  className = "text-center" variant="primary" type="submit" >
             Submit
           </Button>
+
          </div>
         </div>
       </div>
     </Form>
+    <Card  style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={url}/>
+      <Card.Body>
+        <Card.Title className ="text-center">{username}</Card.Title>
+        <Card.Text className ="text-center">
+          {firstname}  {lastname}
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item className ="text-center">{email}</ListGroup.Item>
+      </ListGroup>
+    </Card>
 
+  
     </Container>
+    
   )
 }
 
